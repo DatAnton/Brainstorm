2a39cc634053bfd821fcff71df8db756aadfb81c
Anton Datsenko <tom.dacenko@gmail.com>
2018-08-23 00:42:42 +0300

    email fixes4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,18 +5,7 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.report_mailer.default_url_options = { host: host }
-ActionMailer::Base.smtp_settings = {
-   :address        => 'smtp.sendgrid.net',
-   :port           => '587',
-   :authentication => :plain,
-   :user_name      => ENV['SENDGRID_USERNAME'],
-   :password       => ENV['SENDGRID_PASSWORD'],
-   :domain         => 'heroku.com',
-   :enable_starttls_auto => true
- }
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
