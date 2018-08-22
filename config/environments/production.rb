8cfd5a1c708a3e81838ee9da782ee592c76f2e98
Anton Datsenko <tom.dacenko@gmail.com>
2018-08-23 00:46:48 +0300

    email fixes6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
-config.report_mailer.default_url_options = { host: host }
+config.action_mailer.default_url_options = { host: host }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
