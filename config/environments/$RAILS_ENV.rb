11abf22e79eef9d6ffe82682f1569e78fa238093
Anton Datsenko <tom.dacenko@gmail.com>
2018-08-23 17:12:37 +0300

    email sets

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,8 +2,19 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
-  user_name:            '<username>',
-  password:             '<password>',
+  domain:               'gmail.com',
+  user_name:            '<brainstormapp0@gmail.com>',
+  password:             '<brainSTORM>',
   authentication:       'plain',
   enable_starttls_auto: true }
+
+
+  # ActionMailer::Base.smtp_settings = {
+  #    :address        => 'gmail.com',
+  #    :port           => '587',
+  #    :authentication => :plain,
+  #    :user_name      => ENV['brainstormapp0@gmail.com'],
+  #    :password       => ENV['brainSTORM'],
+  #    :domain         => 'smtp.gmail.com',
+  #    :enable_starttls_auto => true
+  #  }
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+    address:              Settings.smtp.address,
+    port:                 587,
+    domain:               Settings.smtp.domain,
+    user_name:            Settings.smtp.user_name,
+    password:             Settings.smtp.password,
+    openssl_verify_mode:  'none',
+    authentication:       :plain,
+    enable_starttls_auto: true
+}
